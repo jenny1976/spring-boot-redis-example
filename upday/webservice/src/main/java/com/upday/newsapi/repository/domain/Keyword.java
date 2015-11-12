0cfe5c43da71d4e205552c1e163f96021bad97ce
jenny1976 <jennykroete@gmx.de>
2015-11-12 16:15:28 +0100

    add unique_keyword_name constraint

--- upday/webservice/src/main/java/com/upday/newsapi/repository/domain/Keyword.java
+++ upday/webservice/src/main/java/com/upday/newsapi/repository/domain/Keyword.java
@@ -12,11 +12,13 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
 import org.hibernate.annotations.Type;
 
 import org.springframework.data.domain.Persistable;
@@ -27,32 +29,34 @@ import org.springframework.util.CollectionUtils;
  * @author jschulz
  */
 @Entity
-@Table(name = "NEWS_KEYWORD")
+@Table(name = "NEWS_KEYWORD",
+        uniqueConstraints = { @UniqueConstraint(name = "unique_keyword_name", columnNames = {"NAME"}) }
+)
 public class Keyword implements Persistable<Long> {
-    
+
     private static final long serialVersionUID = 4192010154194539491L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(name = "NAME", nullable = false, length = 300)
     private String name;
-    
+
     @Column(name = "DESCRIPTION", length = 500)
     private String description;
-    
+
     @ManyToMany(mappedBy = "keywords", cascade = CascadeType.ALL)
     private List<Article> articles;
-    
+
     @Column(name = "CREATED_ON", nullable = false)
     @Type(type = "org.jadira.usertype.dateandtime.threeten.PersistentLocalDateTime")
     private LocalDateTime createdOn;
-    
+
     @Column(name = "UPDATED_ON", nullable = false)
     @Type(type = "org.jadira.usertype.dateandtime.threeten.PersistentLocalDateTime")
     private LocalDateTime updatedOn;
-    
+
     public Keyword(String name) {
         this.name = name;
     }
@@ -93,7 +97,7 @@ public class Keyword implements Persistable<Long> {
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     public LocalDateTime getCreatedOn() {
         return createdOn;
     }
@@ -109,7 +113,7 @@ public class Keyword implements Persistable<Long> {
     public void setUpdatedOn(LocalDateTime updatedOn) {
         this.updatedOn = updatedOn;
     }
-    
+
     public void addArticle(Article article) {
         if(CollectionUtils.isEmpty(articles)) {
             this.articles = new ArrayList<>();
@@ -122,7 +126,7 @@ public class Keyword implements Persistable<Long> {
     public boolean isNew() {
             return null == getId();
     }
-    
+
     @PrePersist
     public void setDefaultDates() {
         this.updatedOn = LocalDateTime.now();
@@ -177,9 +181,9 @@ public class Keyword implements Persistable<Long> {
 
     @Override
     public String toString() {
-        return "Keyword{" + "id=" + id + ", name=" + name + ", description=" + description 
+        return "Keyword{" + "id=" + id + ", name=" + name + ", description=" + description
                 + ", createdOn=" + createdOn + ", updatedOn=" + updatedOn + '}';
     }
 
-    
+
 }
